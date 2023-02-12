13968849fefed21bb63538f218d288b5fece84a6
Hanna Huzava <guzovaann@gmail.com>
2023-02-12 16:59:23 +0100

    feat: addv US2 -homepage

---
+++ frontend-part/src/components/header/header.js
@@ -0,0 +1,16 @@
+
+import './header.css'
+
+function Header() {
+
+    return (
+        <header>
+            <div className="wrapper">
+            <h2>Logo</h2>
+             
+            </div>   
+        </header>
+    )
+}
+
+export default Header;
