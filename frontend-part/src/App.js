3465da3c2435fa292238aa6041f9eadf0f0c72e6
Hanna Huzava <guzovaann@gmail.com>
2023-02-16 00:55:36 +0100

    feat: add US3 - basic version

--- frontend-part/src/App.js
+++ frontend-part/src/App.js
@@ -1,15 +1,16 @@
 
 import './App.css';
 import Header from './components/header/header';
-import Home from './components/home/home';
+// import Home from './components/home/home';
 import Footer from './components/footer/footer';
+import ProfileForm from './components/profileForm/profileForm';
 
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
+      <ProfileForm></ProfileForm>
       <Footer ></Footer>
     </div>
    
