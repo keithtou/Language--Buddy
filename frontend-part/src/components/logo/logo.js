4d8708cb62c78a0686fd5dff1e13e5436d4677d3
Hafiz Abubaker Nazir <habubakernazir@gmail.com>
2023-02-23 21:04:56 +0100

    done

--- frontend-part/src/components/logo/logo.js
+++ frontend-part/src/components/logo/logo.js
@@ -1,13 +1,12 @@
-import "./logo.css"
-import logo from "../../images/logo_add.png"
+import "./logo.css";
+import logo from "../../images/logo_add.png";
 
 function Logo() {
-    return (
-        <a  href="/" > 
-        <img className="logo_img" src={logo}  width="160" height="140" />
-        </a>
-      
-    )
+  return (
+    <a href="/">
+      <img className="logo_img" src={logo} width="160" height="140" />
+    </a>
+  );
 }
 
 export default Logo;
--- frontend-part/src/components/signIn/signIn.js
+++ frontend-part/src/components/signIn/signIn.js
@@ -1,49 +1,52 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 
-import "./signIn.css"
-import Logo from '../logo/logo';
+import "./signIn.css";
+import Logo from "../logo/logo";
 
 function SignIn() {
-
   const nav = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
-function handleEmailInput(event) {
-  setEmail(event.target.value)
-}
+  function handleEmailInput(event) {
+    setEmail(event.target.value);
+  }
 
-function handlePasswordInput(event) {
-  setPassword(event.target.value)
-}
-
+  function handlePasswordInput(event) {
+    setPassword(event.target.value);
+  }
 
   return (
     <div className="wrapper">
-      <div className="signin__wrapper" >
+      <div className="signin__wrapper">
         <Logo />
-        <div  className="signin__content" >
+        <div className="signin__content">
           <h3>Login to your account</h3>
           <Form>
-           <Form.Group className="mb-3" controlId="formBasicEmail">
-           <Form.Control type="email" placeholder="Enter email" />
-           </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
 
-           <Form.Group className="mb-3" controlId="formBasicPassword">
-           <Form.Control type="password" placeholder="Password"  />
-           </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
 
-           <Button variant="primary" type="submit" className="button signin_login_button">Login</Button>
+            <Button
+              variant="primary"
+              type="submit"
+              className="button signin_login_button"
+            >
+              Login
+            </Button>
           </Form>
         </div>
+      </div>
     </div>
-  </div>
-    
   );
 }
 
