85ad1e6d4f5806ca0bbde0810b54113a5d160ef0
Argenis Arno <agafarno@gmail.com>
2023-02-24 17:57:11 +0100

    initial db conection endpoints

--- Proyect-Backend/server.js
+++ Proyect-Backend/server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const { Pool } = require("pg");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "languagebuddy",
+  password: "postgres",
+  port: 5432,
 });
-
-app.listen(3000, () => console.log("Server is up and running"));
+app.get("/", (req, res) => {
+  pool.query("select * from user_info", (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
