391d441887e33aa9ad352f68e60eb8e287e7c9c4
Argenis Arno <agafarno@gmail.com>
2023-02-24 20:19:02 +0100

    endpoits for profiles

--- Proyect-Backend/server.js
+++ Proyect-Backend/server.js
@@ -16,6 +16,52 @@ app.get("/", (req, res) => {
     res.json(result.rows);
   });
 });
+
+app.post("/signup", function (req, res) {
+  const newName = req.body.name;
+  const newUsername = req.body.username;
+  const newEmail = req.body.email;
+  const newPassword = req.body.password;
+  const newNationality = req.body.nationality;
+  const newGender = req.body.gender;
+  const newLanguage = req.body.language;
+  const newLevel = req.body.level;
+  const newBirthday = req.body.birthday;
+  const newBio = req.body.bio;
+
+  const query =
+    "INSERT INTO user_info (username, password, full_name, date_of_birth, gender, nationality, description, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+
+  //   pool
+  //     .query(query, [
+  //       newUsername,
+  //       newPassword,
+  //       newName,
+  //       newBirthday,
+  //       newGender,
+  //       newNationality,
+  //       newBio,
+  //       newEmail,
+  //     ])
+  //     .then(() => res.send("Profile created!"))
+  //     .catch((e) => console.error(e));
+  // });
+
+  pool
+    .query(query, [
+      "newUsername",
+      "newPassword",
+      newName,
+      "newBirthday",
+      "newGender",
+      "newNationality",
+      "newBio",
+      "newEmail",
+    ])
+    .then(() => res.send("Profile created!"))
+    .catch((e) => console.error(e));
+});
+
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
