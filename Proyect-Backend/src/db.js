56a9016a1d4cbf7ce988bbd0effa7281e9571001
Miguel Desimone <miguel.desimone98@gmail.com>
2023-02-28 22:23:44 +0100

    sign-up backend done

--- Proyect-Backend/src/db.js
+++ Proyect-Backend/src/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'Cali',
-    host: 'localhost',
-    database: 'language_buddy',
-    port: 5432
+  user: "postgres",
+  password: "Postgre",
+  host: "localhost",
+  database: "language_buddy",
+  port: 5432,
 });
 
 module.exports = pool;
