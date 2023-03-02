66c2a2f5123ea460780e8d38b6668d19e4876b22
Hanna Huzava <guzovaann@gmail.com>
2023-03-03 00:42:21 +0100

    feat: show list of students

--- frontend-part/src/components/list/list.js
+++ frontend-part/src/components/list/list.js
@@ -1,44 +1,60 @@
 import "./list.css"
+import { useState } from "react";
+import { useEffect } from "react";
 import Button from "react-bootstrap/esm/Button";
 import { useNavigate } from "react-router-dom";
 import Logo from "../logo/logo";
 import StudentCard from "../studentCard/studentCard";
-// import { state } from "../../data/students";
 
 function List() {
   const nav = useNavigate();
 
-  const cards = async () => {
-    await fetch('http://localhost:4000/users')
-    .then((response) => {
-   
-        if (response.status === 200) {
-            return response.json()
-        } else {
-           return "messsagr"
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Something bad with connection");
         }
-    })
-    .then((data) =>  data)
-}
+        return res.json();
+      })
+      .then(
+        data => {
+          setStudents(data);
+          setLoading(false);
+        },
+        error => {
+          setError(error.toString());
+          console.log(error);
+          setLoading(false);
+        }
+      );
+  }, []);
+
+ 
 
     return (
         <div>
             <div className="wrapper">
-                <div className="main_header">
+              <div className="main_header">
                     <Logo />
                     <div className="button_container">
                     <Button className="button profile_button" type="submit">Profile</Button>
                     <Button className="button logout_button" type="submit" onClick={() => nav("/")}>Logout</Button>
                     </div>
-                </div>
+              </div>
 
-                <Button className="button filter_button" type="submit">Filter</Button>
+              <Button className="button filter_button" type="submit">Filter</Button>
             
-                <div className="card-container">
-                    {cards.map((card) => {
-                        return <StudentCard card={card} key={Math.random()} />;
-                    })}
-                </div>
+                
+              <div className="card-container">
+                  { loading ? ( <p>The list of students is loading...</p> ) : error != "" ? ( <p>{error}</p> ) : (
+                      students.map((student, index) => <StudentCard students={student} key={index} />)
+                  )}
+              </div>
               
             </div>
         </div>
