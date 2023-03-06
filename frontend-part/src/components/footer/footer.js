c718844dc62aba78065575499d8f9c6a28e78485
Hanna Huzava <guzovaann@gmail.com>
2023-03-06 12:31:46 +0100

    fix footer css

--- frontend-part/src/components/footer/footer.js
+++ frontend-part/src/components/footer/footer.js
@@ -1,43 +1,25 @@
 import React from "react";
-import { useState } from "react";
 import "./footer.css"
-import Logo from "../logo/logo";
 import logo_languagelab_transparent from '../../images/logo_languagelab_transparent.png';
 import logo_occ from '../../images/logo_occ.png';
 import logo_Transparent_Migracode from '../../images/logo_Transparent_Migracode.png';
-import { Link } from "react-router-dom";
-
 
 function Footer() {
     return (
         <footer>
+          <div className="wrapper">
+
           <div className="footer-wrapper">
-      <div className="logos">
-          <img src={logo_languagelab_transparent} alt="Language lab logo" width="110" height="88"  />
-          <div className="logos">
-          <img src={logo_occ} alt="logo OCC"  width="130" height="88"/>
-          </div>
-          <div className="logos">
-          <img src={logo_Transparent_Migracode} alt= "logo Migracode" width="130" height="88"/>
-          </div>
-        
-      </div>
-    
-            <div className="copyright">
-            <p> &copy;{new Date().getFullYear()} Language Buddy | All rights reserved</p>
-            </div>
-                
-            </div>
-            <div className="language-selector">
+    <div className="language-selector">
         <p>Select Language:</p>
         <select>
           <option value="en">English</option>
           <option value="es">Español</option>
         </select>
+         </div>
 
-      </div>
-
-      <nav>
+         
+         <nav>
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="#">About Us</a></li>
@@ -45,8 +27,30 @@ function Footer() {
           <li><a href="#">FAQ</a></li>
         </ul>
       </nav>
+          <div className="logos">
+          <ul>
+          <li><a href="https://openculturalcenter.org/language-lab/"> <img  className="lab" src={logo_languagelab_transparent} alt="Language lab logo" width="100%" height="100"  /></a></li>
+          <li><a href="https://openculturalcenter.org/"> <img  className="occ" src={logo_occ} alt="logo OCC"  width="100%" height="100"/></a></li>
+          <li><a href="https://migracode.openculturalcenter.org/"><img  className="migracode" src={logo_Transparent_Migracode} alt= "logo Migracode" width="100%" height="100"/></a></li>
+        </ul>
 
-        </footer>
+
+        </div>
+      
+            </div>
+
+
+         
+
+     
+
+
+      <div className="copyright">
+            <p> &copy;{new Date().getFullYear()} Language Buddy | All rights reserved</p>
+          </div>
+
+      </div>
+      </footer>
        
   
     )
