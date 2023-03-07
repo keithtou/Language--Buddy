ed8023c3176df828e1a66c7c5a59e5e2e5fc7df2
Hanna Huzava <guzovaann@gmail.com>
2023-03-07 21:19:45 +0100

    f

--- frontend-part/src/components/studentCard/studentCard.js
+++ frontend-part/src/components/studentCard/studentCard.js
@@ -2,22 +2,16 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import "./studentCard.css";
 import image from "../../images/Profile-PNG-File.png"
-import {languageList } from "../../data/languagesList";
-import { levels } from '../../data/levels';
 
 function StudentCard(props) {
 
-    let languageName = languageList.find(el => el.id === props.students["language"]).name;
-    let languageLevel = levels.find(el => el.id === props.students["language_level"]).name;
-
-   
   return (
     <Card className="student_card" id={props.students["id"]}>
       <Card.Img variant="top" src={image} />
       <Card.Body className="card_body">
         <Card.Title className="card_title">{props.students["username"]}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{props.students["nationality"]}</Card.Subtitle>
-        <Card.Subtitle className="mb-3 text-muted">{languageName} - {languageLevel}</Card.Subtitle>
+        <Card.Subtitle className="mb-3 text-muted">{props.students["language_name"]} - {props.students["levels"]}</Card.Subtitle>
         <Card.Text>{props.students["description"]}</Card.Text>
         <Button className="button send_button" type="submit">Send Request</Button>
       </Card.Body>
