c5b6dab1287362ecc69297cd453a26506d5eec73
Hanna Huzava <guzovaann@gmail.com>
2023-03-15 00:11:26 +0100

    fix profile link

--- frontend-part/src/components/dropdownButtons/dropdownButtons.js
+++ frontend-part/src/components/dropdownButtons/dropdownButtons.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import { useNavigate } from 'react-router-dom';
 
 
 function DropdownButtons() {
+  let nav = useNavigate();
     return (
         <DropdownButton id="dropdown-basic-button" title="Profile">
-          <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+          <Dropdown.Item onClick={() => nav("/profile")}>Profile</Dropdown.Item>
           <Dropdown.Item href="/" onClick={()=> localStorage.removeItem("jwtToken")}>Log out</Dropdown.Item>
         </DropdownButton>
       );
