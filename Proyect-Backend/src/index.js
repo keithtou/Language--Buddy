6af5a92dfe2a6d5ac8062b2191b920cb879c60af
camilla.colomboserri <camilla.colomboserri@wallapop.com>
2023-03-14 21:51:10 +0100

    Serve the FE from a BE endpoint

--- Proyect-Backend/src/index.js
+++ Proyect-Backend/src/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const path = require('path');
 
 // Middlewares
 app.use(cors());
@@ -16,9 +17,10 @@ app.use(bodyParser.json());
 // Routes
 app.use(routes);
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to language buddy Migracode!!!." });
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 4000;
