68f5a9be5a6519e0ba8889ddd45428048c5db181
Hanna Huzava <guzovaann@gmail.com>
2023-03-15 00:33:29 +0100

    fix footer

--- frontend-part/src/components/footer/footer.js
+++ frontend-part/src/components/footer/footer.js
@@ -24,10 +24,10 @@ function Footer() {
              
               <nav>
                 <ul>
-                  <li><a onClick={()=> nav("/aboutus")}>About Us</a></li>
+                  <li onClick={()=> nav("/aboutus")}>About Us</li>
                   <li><a href="mailto:languages@openculturalcenter.org" target="_blank">Contact Us</a></li>
                   <li><a href="https://www.google.com/maps/place/Open+Cultural+Center+Barcelona/@41.3860019,2.1823493,15z/data=!4m6!3m5!1s0x12a4a2fdd0e3846f:0x5b7de3c819c66eb0!8m2!3d41.3860059!4d2.1823578!16s%2Fg%2F11f3m91v5t" target="_blank">Carrer del Rec 27, 08003 Barcelona</a></li>
-                  <li><a onClick={()=> nav("/faq")}>FAQ</a></li>
+                  <li onClick={()=> nav("/faq")}>FAQ</li>
                 </ul>
               </nav>
 
